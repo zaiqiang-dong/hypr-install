ea87728b56d1c87ec7203214d3e183d358ff34cf
Zaiqiang Dong <dongzq@autoai.com>
2025-06-25 10:42:12 +0800

    add config

---
+++ scripts/hyprutil.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+git clone https://github.com/hyprwm/hyprutils.git
+cd hyprutils/
+cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build
+cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf NPROCESSORS_CONF`
+sudo cmake --install build
